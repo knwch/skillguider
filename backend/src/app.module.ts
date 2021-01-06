7645c427ffbe9ae412b6c042670b3704576c9e67
zkorra <korrawich.dev@outlook.com>
2021-01-06 18:17:31 +0700

    connected to local mongo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/skillguider'),
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
