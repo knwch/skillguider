9e6120a0ca81b8666c01e8932c18e4f7864bc894
zkorra <korrawich.dev@outlook.com>
2021-01-06 18:17:50 +0700

    init service

---
+++ backend/src/category/category.service.ts
@@ -0,0 +1,22 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Category, CategoryDocument } from './category.schema';
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectModel(Category.name)
+    private readonly categoryModel: Model<CategoryDocument>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const createdCategory = new this.categoryModel(createCategoryDto);
+    return createdCategory.save();
+  }
+
+  async findAll(): Promise<Category[]> {
+    return this.categoryModel.find().exec();
+  }
+}
