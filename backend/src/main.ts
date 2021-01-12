1f7411c6250172e68892c39e3792c6cd63663565
zkorra <korrawich.dev@outlook.com>
2021-01-12 14:53:29 +0700

    setup api doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('SkillGuider App')
+    .setDescription('The SkillGuider API description')
+    .setVersion('1.0')
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
