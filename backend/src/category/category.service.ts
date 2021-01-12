08b4dd5e4f245d53c767272be5e24c614e674080
zkorra <korrawich.dev@outlook.com>
2021-01-12 14:54:16 +0700

    updated CRUD func

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from './schema/category.schema';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class CategoryService {
@@ -12,49 +11,30 @@ export class CategoryService {
     private readonly CategoryModel: Model<CategoryDocument>,
   ) {}
 
-  async getAllCategories(): Promise<any> {
-    return await this.CategoryModel.find().exec();
-  }
-
-  // getCategoryById(id: string): Category {
-  //   return this.categories.find((category) => category.id === id);
-  // }
-
   async createCategory(categoryData: CreateCategoryDto): Promise<Category> {
     const createdCategory = new this.CategoryModel(categoryData);
     return createdCategory.save();
   }
 
-  // deleteCategory(id: string) {
-  //   this.categories = this.categories.filter((category) => category.id !== id);
-  // }
+  async getAllCategories(): Promise<any> {
+    return await this.CategoryModel.find().exec();
+  }
 
-  // constructor(
-  //   @InjectModel(Category.name)
-  //   private readonly categoryModel: Model<CategoryDocument>,
-  // ) {}
+  async getCategoryById(id: string): Promise<Category> {
+    const category = await this.CategoryModel.findById(id).exec();
+    return category;
+  }
 
-  // async create(createCategoryDto: CreateCategoryDto): Promise<any> {
-  //   const createdCategory = new this.categoryModel(createCategoryDto);
-  //   return createdCategory.save();
-  // }
+  async updateCategory(
+    id: string,
+    categoryData: CreateCategoryDto,
+  ): Promise<Category> {
+    return await this.CategoryModel.findByIdAndUpdate(id, categoryData, {
+      new: true,
+    });
+  }
 
-  // async findAll(): Promise<any> {
-  //   return await this.categoryModel.find().exec();
-  // }
-
-  // async update(id, createCategoryDto: CreateCategoryDto): Promise<any> {
-  //   // return await this.categoryModel.findById(id);
-  //   const updateCategory = new this.categoryModel(createCategoryDto);
-  //   return await this.categoryModel.findByIdAndUpdate(
-  //     id,
-  //     {
-  //       category: updateCategory.category,
-  //       skills_category: updateCategory.skills_category,
-  //     },
-  //     { useFindAndModify: false },
-  //   );
-  // }
-
-  // return await this.categoryModel.updateOne(id, updateCategory);
+  async deleteCategory(id: string): Promise<any> {
+    return await this.CategoryModel.findByIdAndRemove(id);
+  }
 }
