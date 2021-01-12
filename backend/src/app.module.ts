f706f41842fee0f539ff1116d0ed831eb3c4f28b
zkorra <korrawich.dev@outlook.com>
2021-01-12 14:53:47 +0700

    config mongoose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import * as mongoose from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoryModule } from './category/category.module';
+
+mongoose.set('useFindAndModify', false);
 
 @Module({
   imports: [
