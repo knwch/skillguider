3a2401e13c3b4020d4a45b2e39bedd41540ab375
knwch <t.korrawich@protonmail.com>
2021-01-12 14:54:16 +0700

    updated CRUD func

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -7,7 +7,6 @@ import {
   UsePipes,
   Res,
   Query,
-  Param,
   NotFoundException,
   HttpStatus,
   ValidationPipe,
@@ -15,24 +14,12 @@ import {
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { Category } from './schema/category.schema';
 
 @Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
-  @Get()
-  async getAllCategories(@Res() res) {
-    const data = await this.categoryService.getAllCategories();
-    return res.status(HttpStatus.OK).json(data);
-  }
-
-  // @Get('/:id')
-  // getCategoryById(@Param('id') id: string) {
-  //   return this.categoryService.getCategoryById(id);
-  // }
-
-  @Post()
+  @Post('create')
   @UsePipes(ValidationPipe)
   async createCategory(@Res() res, @Body() categoryData: CreateCategoryDto) {
     const data = await this.categoryService.createCategory(categoryData);
@@ -42,59 +29,40 @@ export class CategoryController {
     });
   }
 
-  // @Delete('/:id')
-  // deleteCategory(@Param('id') id: string) {
-  //   this.categoryService.deleteCategory(id);
-  // }
+  @Get('all')
+  async getAllCategories(@Res() res) {
+    const data = await this.categoryService.getAllCategories();
+    return res.status(HttpStatus.OK).json(data);
+  }
 
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // async create(@Body() createCategoryDto: CreateCategoryDto) {
-  //   await this.categoryService.createCategory(createCategoryDto);
-  // }
+  @Get('id')
+  async getCategoryById(@Res() res, @Query('id') id: string) {
+    const data = await this.categoryService.getCategoryById(id);
+    if (!data) throw new NotFoundException('Category does not exist!');
+    return res.status(HttpStatus.OK).json(data);
+  }
 
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // async createCategory(
-  //   @Res() res,
-  //   @Body() createCategoryDto: CreateCategoryDto,
-  // ) {
-  //   const lists = await this.categoryService.create(createCategoryDto);
-  //   return res.status(HttpStatus.OK).json({
-  //     message: 'Post has been created successfully',
-  //     lists,
-  //   });
-  // }
+  @Put('update')
+  async updateCategory(
+    @Res() res,
+    @Query('id') id: string,
+    @Body() categoryData: CreateCategoryDto,
+  ) {
+    const data = await this.categoryService.updateCategory(id, categoryData);
+    if (!data) throw new NotFoundException('Category does not exist!');
+    return res.status(HttpStatus.OK).json({
+      message: 'Category has been successfully updated',
+      data,
+    });
+  }
 
-  // @Get()
-  // async findAll(@Res() res) {
-  //   const lists = await this.categoryService.findAll();
-  //   return res.status(HttpStatus.OK).json(lists);
-  // }
-
-  // @Get()
-  // async findAll(): Promise<Category[]> {
-  //   return this.categoryService.findAll();
-  // }
-
-  // async update(
-  //   @Param('id') id: string,
-  //   @Body() categoryData: CreateCategoryDto,
-  // ) {
-  //   return this.categoryService.update(id, categoryData);
-  // }
-
-  // @Put()
-  // async updateCategory(
-  //   @Res() res,
-  //   @Query('id') id: string,
-  //   @Body() updateCategoryDto: CreateCategoryDto,
-  // ) {
-  //   const lists = await this.categoryService.update(id, updateCategoryDto);
-  //   if (!lists) throw new NotFoundException('Id does not exist!');
-  //   return res.status(HttpStatus.OK).json({
-  //     message: 'Post has been successfully updated',
-  //     lists,
-  //   });
-  // }
+  @Delete('delete')
+  async deleteCategory(@Res() res, @Query('id') id: string) {
+    const data = await this.categoryService.deleteCategory(id);
+    if (!data) throw new NotFoundException('Category does not exist!');
+    return res.status(HttpStatus.OK).json({
+      message: 'Category has been deleted',
+      data,
+    });
+  }
 }
