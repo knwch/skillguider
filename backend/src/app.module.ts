cc02db076aab7f071e4a9b16f09b9a8568fb717e
knwch <t.korrawich@protonmail.com>
2021-01-14 15:38:46 +0700

    add job module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import * as mongoose from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
+import { JobModule } from './job/job.module';
 
 mongoose.set('useFindAndModify', false);
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/skillguider'),
-    CategoryModule,
     AuthModule,
+    CategoryModule,
+    JobModule,
   ],
 })
 export class AppModule {}
