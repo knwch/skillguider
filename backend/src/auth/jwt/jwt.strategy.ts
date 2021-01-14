97f5ad909986a9318738d35cd34bef5926840210
zkorra <korrawich.dev@outlook.com>
2021-01-14 13:12:20 +0700

    jwt guard

---
+++ backend/src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { JwtPayload } from './jwt-payload.interface';
+import { User, UserDocument } from '../schema/user.schema';
+import { SECRET } from '../../config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectModel('User')
+    private readonly UserModel: Model<UserDocument>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.UserModel.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
