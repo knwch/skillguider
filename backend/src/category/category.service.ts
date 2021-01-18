7c7c9b266aabe1abcd12b701c067b033345abe8e
zkorra <korrawich.dev@outlook.com>
2021-01-18 16:32:16 +0700

    created category condition

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,9 +1,11 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from './schema/category.schema';
 import { JobDocument } from '../job/schema/job.schema';
+import { SkillDocument } from '../skill/schema/skill.schema';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class CategoryService {
@@ -12,10 +14,35 @@ export class CategoryService {
     private readonly CategoryModel: Model<CategoryDocument>,
     @InjectModel('Job')
     private readonly JobModel: Model<JobDocument>,
+    @InjectModel('Skill')
+    private readonly SkillModel: Model<SkillDocument>,
   ) {}
 
   async createCategory(categoryData: CreateCategoryDto): Promise<Category> {
     const createdCategory = new this.CategoryModel(categoryData);
+    let modelSkills: any;
+
+    const { skillset } = createdCategory;
+
+    if (skillset?.length) {
+      // convert skill_id from object to string array
+      const convertedSkillIds = skillset.map((skill) => {
+        return skill.skill_id;
+      });
+
+      modelSkills = await this.SkillModel.find({
+        _id: { $in: convertedSkillIds },
+      });
+
+      if (skillset?.length !== modelSkills.length) {
+        const errors = { skillset: 'Some skills do not exist.' };
+        throw new HttpException(
+          { message: 'Input data validation failed', errors },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    }
+
     return createdCategory.save();
   }
 
@@ -29,14 +56,14 @@ export class CategoryService {
     return category;
   }
 
-  async updateCategory(
-    id: string,
-    categoryData: CreateCategoryDto,
-  ): Promise<Category> {
-    return await this.CategoryModel.findByIdAndUpdate(id, categoryData, {
-      new: true,
-    });
-  }
+  // async updateCategory(
+  //   id: string,
+  //   categoryData: CreateCategoryDto,
+  // ): Promise<Category> {
+  //   return await this.CategoryModel.findByIdAndUpdate(id, categoryData, {
+  //     new: true,
+  //   });
+  // }
 
   async deleteCategory(id: string): Promise<any> {
     const categoryDeleted = await this.CategoryModel.deleteOne({ _id: id });
