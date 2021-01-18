3e70342e7045afc77caa56a76772eb2f4b6d4ca6
zkorra <korrawich.dev@outlook.com>
2021-01-18 16:28:31 +0700

    created module

---
+++ backend/src/skill/skill.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SkillController } from './skill.controller';
+import { SkillService } from './skill.service';
+import { SkillSchema } from './schema/skill.schema';
+import { AuthModule } from '../auth/auth.module';
+import { CategorySchema } from '../category/schema/category.schema';
+import { JobSchema } from '../job/schema/job.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Skill', schema: SkillSchema },
+      { name: 'Job', schema: JobSchema },
+      { name: 'Category', schema: CategorySchema },
+    ]),
+    AuthModule,
+  ],
+  providers: [SkillService],
+  controllers: [SkillController],
+})
+export class SkillModule {}
