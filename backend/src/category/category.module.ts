fbc42d75b3741c6f85bf4548a70e6f6850745c81
knwch <t.korrawich@protonmail.com>
2021-01-18 16:31:32 +0700

    imported skill schema

--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -5,12 +5,14 @@ import { CategoryService } from './category.service';
 import { CategorySchema } from './schema/category.schema';
 import { AuthModule } from '../auth/auth.module';
 import { JobSchema } from '../job/schema/job.schema';
+import { SkillSchema } from '../skill/schema/skill.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Category', schema: CategorySchema },
       { name: 'Job', schema: JobSchema },
+      { name: 'Skill', schema: SkillSchema },
     ]),
     AuthModule,
   ],
