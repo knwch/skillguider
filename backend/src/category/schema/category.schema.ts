ae25f93e20626643f5b81ec7c0ac02878e24e94f
knwch <t.korrawich@protonmail.com>
2021-01-19 10:46:25 +0700

    fixed skillset prop

--- backend/src/category/schema/category.schema.ts
+++ backend/src/category/schema/category.schema.ts
@@ -3,13 +3,20 @@ import { Document, Types } from 'mongoose';
 
 export type CategoryDocument = Category & Document;
 
+@Schema({ _id: false })
+class Skill {
+  @Prop()
+  skill_id: Types.ObjectId;
+}
+export const SkillSchema = SchemaFactory.createForClass(Skill);
+
 @Schema()
 export class Category {
   @Prop()
   title: string;
 
-  @Prop()
-  skillset: [{ skill_id: Types.ObjectId }];
+  @Prop({ type: [SkillSchema], default: [] })
+  skillset: Skill[];
 }
 
 export const CategorySchema = SchemaFactory.createForClass(Category);
