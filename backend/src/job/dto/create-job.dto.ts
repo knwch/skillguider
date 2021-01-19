dcf770280b9ca37040e8fcc2c22f93c368fc3973
knwch <t.korrawich@protonmail.com>
2021-01-19 13:57:09 +0700

    fixed dto

--- backend/src/job/dto/create-job.dto.ts
+++ backend/src/job/dto/create-job.dto.ts
@@ -1,16 +1,40 @@
-import { IsNotEmpty, IsOptional, IsMongoId } from 'class-validator';
+import { Types } from 'mongoose';
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  IsMongoId,
+  IsArray,
+  ValidateNested,
+} from 'class-validator';
+
+class Skill {
+  @IsNotEmpty()
+  @IsMongoId()
+  readonly skill_id: Types.ObjectId;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly priority: string;
+}
 
 export class CreateJobDto {
   @IsNotEmpty()
+  @IsString()
   readonly title: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly description: string;
 
   @IsNotEmpty()
   @IsMongoId()
-  readonly category_id: string;
+  readonly category_id: Types.ObjectId;
 
   @IsOptional()
-  readonly skillset: [];
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Skill)
+  readonly skillset: [Skill];
 }
