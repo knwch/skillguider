4b380862eb746d951b1a863ed53406dc1f1deb4f
knwch <t.korrawich@protonmail.com>
2021-01-20 16:18:12 +0700

    changed type array of string

--- backend/src/skill/dto/create-skill.dto.ts
+++ backend/src/skill/dto/create-skill.dto.ts
@@ -1,12 +1,14 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSkillDto {
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   readonly title: string;
 
   @ApiProperty()
   @IsOptional()
+  @IsString({ each: true })
   readonly keywords: [];
 }
