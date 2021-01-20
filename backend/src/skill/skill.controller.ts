db300fecbf66d6c4774d48cd5e7a963657234042
zkorra <korrawich.dev@outlook.com>
2021-01-20 15:29:45 +0700

    add skill docs

--- backend/src/skill/skill.controller.ts
+++ backend/src/skill/skill.controller.ts
@@ -16,10 +16,24 @@ import { AuthGuard } from '@nestjs/passport';
 import { SkillService } from './skill.service';
 import { CreateSkillDto } from './dto/create-skill.dto';
 
+import {
+  ApiBearerAuth,
+  ApiResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+
+@ApiTags('skill')
 @Controller('skill')
 export class SkillController {
   constructor(private skillService: SkillService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create new skill' })
+  @ApiResponse({
+    status: 201,
+    description: 'Skill has been successfully created.',
+  })
   @Post('create')
   @UseGuards(AuthGuard())
   @UsePipes(ValidationPipe)
@@ -32,18 +46,36 @@ export class SkillController {
     });
   }
 
+  @ApiOperation({ summary: 'Get all skills' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all skills.',
+  })
   @Get('all')
   async getAllSkills(@Res() res) {
     const data = await this.skillService.getAllSkills();
     return res.status(HttpStatus.OK).json({ statusCode: HttpStatus.OK, data });
   }
 
+  @ApiOperation({ summary: 'Get skill by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return skill by id.',
+  })
+  @ApiResponse({ status: 404, description: 'Skill not found.' })
   @Get('id')
   async getSkillById(@Res() res, @Query('id') id: string) {
     const data = await this.skillService.getSkillById(id);
     return res.status(HttpStatus.OK).json({ statusCode: HttpStatus.OK, data });
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update skill' })
+  @ApiResponse({
+    status: 200,
+    description: 'Skill has been successfully updated.',
+  })
+  @ApiResponse({ status: 404, description: 'Skill not found.' })
   @Put('update')
   @UseGuards(AuthGuard())
   @UsePipes(ValidationPipe)
@@ -60,6 +92,13 @@ export class SkillController {
     });
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete skill' })
+  @ApiResponse({
+    status: 200,
+    description: 'Skill has been deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Skill not found.' })
   @Delete('delete')
   @UseGuards(AuthGuard())
   async deleteCategory(@Res() res, @Query('id') id: string) {
