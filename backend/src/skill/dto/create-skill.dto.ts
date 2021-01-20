4827bf1731b7a93482818cd6ad9c08e33b610c6c
knwch <t.korrawich@protonmail.com>
2021-01-20 14:58:28 +0700

    add schema docs

--- backend/src/skill/dto/create-skill.dto.ts
+++ backend/src/skill/dto/create-skill.dto.ts
@@ -1,9 +1,12 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSkillDto {
+  @ApiProperty()
   @IsNotEmpty()
   readonly title: string;
 
+  @ApiProperty()
   @IsOptional()
   readonly keywords: [];
 }
