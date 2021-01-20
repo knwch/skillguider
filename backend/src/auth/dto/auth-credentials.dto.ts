21ab6295f4b269c6a21ba957060c3322433552d5
knwch <t.korrawich@protonmail.com>
2021-01-20 16:17:54 +0700

    validated string

--- backend/src/auth/dto/auth-credentials.dto.ts
+++ backend/src/auth/dto/auth-credentials.dto.ts
@@ -1,12 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthCredentialsDto {
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   readonly username: string;
 
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   readonly password: string;
 }
