3a264312232bc5e30538d2576e2fedebde217208
knwch <t.korrawich@protonmail.com>
2021-02-10 16:18:53 +0700

    set createindex

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
@@ -7,6 +7,7 @@ import { JobModule } from './job/job.module';
 import { SkillModule } from './skill/skill.module';
 
 mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 @Module({
   imports: [
