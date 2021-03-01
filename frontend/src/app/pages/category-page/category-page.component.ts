dcc6922eaa458794bf94c937baf4c7d0b1d5ca2a
knwch <t.korrawich@protonmail.com>
2021-03-01 16:32:42 +0700

    add category page

---
+++ frontend/src/app/pages/category-page/category-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-page',
+  templateUrl: './category-page.component.html',
+  styleUrls: ['./category-page.component.sass']
+})
+export class CategoryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
