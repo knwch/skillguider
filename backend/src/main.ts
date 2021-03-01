0dc557e60396452471268f8376813e6204d4ce4f
knwch <t.korrawich@protonmail.com>
2021-03-01 16:31:30 +0700

    enabled cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
