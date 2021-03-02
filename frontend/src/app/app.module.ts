f4b68b51ce187bc83b8b8afe5fa1fb3cab306092
zkorra <korrawich.dev@outlook.com>
2021-03-02 15:39:19 +0700

    import ngxs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,19 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryPageComponent } from './pages/category-page/category-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MaterialModule } from './material.module';
 import { TopbarComponent } from './components/topbar/topbar.component';
 
-import { StoreModule } from '@ngrx/store';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+import { CategoryState } from './states/category.state';
 
 @NgModule({
   declarations: [AppComponent, CategoryPageComponent, TopbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxsModule.forRoot([CategoryState]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
     BrowserAnimationsModule,
     MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
