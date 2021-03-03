4753133d7ab6e9d99cd2aee94fbd1043e41ef88b
zkorra <korrawich.dev@outlook.com>
2021-03-03 16:57:21 +0700

    changed top bar

--- frontend/src/app/components/topbar/topbar.component.ts
+++ frontend/src/app/components/topbar/topbar.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.sass']
+  styleUrls: ['./topbar.component.sass'],
 })
 export class TopbarComponent implements OnInit {
+  items: MenuItem[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): any {
+    this.items = [
+      {
+        label: 'Dashboard',
+        routerLink: '/dashboard',
+      },
+      // {
+      //   icon: 'pi pi-fw pi-user',
+      //   items: [
+      //     {
+      //       label: 'Profile',
+      //       icon: 'my-margin-left pi pi-fw pi-user',
+      //     },
+      //     {
+      //       label: 'Logout',
+      //       styleClass: 'p-ml-6',
+      //       icon: 'my-margin-left pi pi-fw pi-sign-out',
+      //     },
+      //   ],
+      // },
+    ];
   }
-
 }
