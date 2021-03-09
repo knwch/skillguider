73621d18e9c473816cb28c8a25ddc6b30d76ec86
knwch <t.korrawich@protonmail.com>
2021-03-09 18:17:12 +0700

    job route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryPageComponent } from './pages/category-page/category-page.component';
+import { JobPageComponent } from './pages/job-page/job-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'category', pathMatch: 'full' },
   { path: 'category', component: CategoryPageComponent },
+  { path: 'job', component: JobPageComponent },
 ];
 
 @NgModule({
