d94e342ab74f6c3757366ce027d0e4b05092f4a3
knwch <t.korrawich@protonmail.com>
2021-03-10 17:58:59 +0700

    get job by cate

--- frontend/src/app/actions/job.action.ts
+++ frontend/src/app/actions/job.action.ts
@@ -4,6 +4,12 @@ export class GetJobs {
   static readonly type = '[Job] Get';
 }
 
+export class GetJobsByCategory {
+  static readonly type = '[Job] Get by Category';
+
+  constructor(public query: string) {}
+}
+
 export class SearchJobs {
   static readonly type = '[Job] Search';
 
