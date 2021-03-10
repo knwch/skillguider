e8cf8297692b24e8e955b9b685a777e0250e1172
knwch <t.korrawich@protonmail.com>
2021-03-10 17:58:28 +0700

    manged routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryPageComponent } from './pages/category-page/category-page.component';
 import { JobPageComponent } from './pages/job-page/job-page.component';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'category', pathMatch: 'full' },
-  { path: 'category', component: CategoryPageComponent },
-  { path: 'job', component: JobPageComponent },
+  {
+    path: 'category',
+    children: [
+      { path: ':id', component: JobPageComponent },
+      { path: '', component: CategoryPageComponent, pathMatch: 'full' },
+    ],
+  },
+  { path: '**', component: NotfoundPageComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
