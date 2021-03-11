615d9e292b8fde75f159816fd427184d165871a4
knwch <t.korrawich@protonmail.com>
2021-03-11 17:27:40 +0700

    add myskill page

--- frontend/src/app/pages/myskill-page/myskill-page.component.ts
+++ frontend/src/app/pages/myskill-page/myskill-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { SkillState } from '../../states/skill.state';
+import { Skill } from '../../models/skill.model';
+import { SearchSkills } from '../../actions/skill.action';
 
 @Component({
   selector: 'app-myskill-page',
   templateUrl: './myskill-page.component.html',
-  styleUrls: ['./myskill-page.component.scss']
+  styleUrls: ['./myskill-page.component.scss'],
 })
 export class MyskillPageComponent implements OnInit {
+  @Select(SkillState.getSkillList)
+  skills: Observable<any> | undefined;
 
-  constructor() { }
+  text: any;
 
-  ngOnInit(): void {
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {}
+
+  onSearch(event: string): any {
+    const { query }: any = event;
+    this.store.dispatch(new SearchSkills(query));
   }
-
 }
