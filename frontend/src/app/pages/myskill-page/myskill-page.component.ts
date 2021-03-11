4cce6f17fa42c78fc1db41f453fc2a50a71e68bf
zkorra <korrawich.dev@outlook.com>
2021-03-11 15:07:14 +0700

    new component

---
+++ frontend/src/app/pages/myskill-page/myskill-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myskill-page',
+  templateUrl: './myskill-page.component.html',
+  styleUrls: ['./myskill-page.component.scss']
+})
+export class MyskillPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
