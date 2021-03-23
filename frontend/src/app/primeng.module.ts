12e016ec773fc3adb9587e424236984ee5070054
zkorra <korrawich.dev@outlook.com>
2021-03-23 15:05:38 +0700

    add tabmenu module

--- frontend/src/app/primeng.module.ts
+++ frontend/src/app/primeng.module.ts
@@ -9,6 +9,7 @@ import { AutoCompleteModule } from 'primeng/autocomplete';
 import { DialogModule } from 'primeng/dialog';
 import { DividerModule } from 'primeng/divider';
 import { CardModule } from 'primeng/card';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @NgModule({
   imports: [CommonModule],
@@ -22,6 +23,7 @@ import { CardModule } from 'primeng/card';
     DialogModule,
     DividerModule,
     CardModule,
+    TabMenuModule,
   ],
 })
 export class PrimeNgModule {}
