38c35365623043f49e8c8831422b19f1be7a8df2
knwch <t.korrawich@protonmail.com>
2021-03-29 16:37:19 +0700

    import new components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+// PrimeNG Modules
 import { PrimeNgModule } from './primeng.module';
 
+// Component
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,14 +19,20 @@ import { JobPageComponent } from './pages/job-page/job-page.component';
 import { ResultPageComponent } from './pages/result-page/result-page.component';
 import { LearnPageComponent } from './pages/learn-page/learn-page.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { AdminCategoryComponent } from './components/layout/admin-category/admin-category.component';
+import { AdminJobComponent } from './components/layout/admin-job/admin-job.component';
+import { AdminSkillComponent } from './components/layout/admin-skill/admin-skill.component';
 
+// NGXS
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
+// State
 import { CategoryState } from './states/category.state';
 import { JobState } from './states/job.state';
 import { SkillState } from './states/skill.state';
+
 
 @NgModule({
   declarations: [
@@ -39,6 +46,9 @@ import { SkillState } from './states/skill.state';
     ResultPageComponent,
     LearnPageComponent,
     AdminPageComponent,
+    AdminCategoryComponent,
+    AdminJobComponent,
+    AdminSkillComponent,
   ],
   imports: [
     BrowserModule,
