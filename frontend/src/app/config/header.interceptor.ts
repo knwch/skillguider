8fcd88cea62c3107274b207f046e01c306cbd035
knwch <t.korrawich@protonmail.com>
2021-03-30 14:38:51 +0700

    add http interceptor

---
+++ frontend/src/app/config/header.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngxs/store';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpResponse,
+  HttpRequest,
+  HttpHandler,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthState } from '../auth/auth.state';
+
+@Injectable()
+export class HeaderInterceptor implements HttpInterceptor {
+  constructor(private store: Store) {}
+
+  intercept(
+    httpRequest: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.store.selectSnapshot(AuthState.token);
+    const Authorization = `Bearer ${token}`;
+    return next.handle(httpRequest.clone({ setHeaders: { Authorization } }));
+  }
+}
