f9e5ebab9d8cc9170aa2860c75d61c053a1f3b7f
knwch <t.korrawich@protonmail.com>
2021-03-30 12:56:48 +0700

    auth routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { CategoryPageComponent } from './pages/category-page/category-page.component';
 import { JobPageComponent } from './pages/job-page/job-page.component';
 import { MyskillPageComponent } from './pages/myskill-page/myskill-page.component';
@@ -8,6 +9,7 @@ import { ResultPageComponent } from './pages/result-page/result-page.component';
 import { LearnPageComponent } from './pages/learn-page/learn-page.component';
 import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'category', pathMatch: 'full' },
@@ -21,12 +23,14 @@ const routes: Routes = [
   { path: 'myskill', component: MyskillPageComponent },
   { path: 'result', component: ResultPageComponent },
   { path: 'learn', component: LearnPageComponent },
-  { path: 'admin', component: AdminPageComponent },
+  { path: 'auth', component: AuthPageComponent },
+  { path: 'admin', canActivate: [AuthGuard], component: AdminPageComponent },
   { path: '**', component: NotfoundPageComponent },
 ];
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
