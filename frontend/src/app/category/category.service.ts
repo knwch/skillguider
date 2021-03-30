5110896e2aa58f2e9919240989a87df3b7c73b4c
knwch <t.korrawich@protonmail.com>
2021-03-30 14:38:33 +0700

    remove mock header

--- frontend/src/app/category/category.service.ts
+++ frontend/src/app/category/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Category } from './category.model';
 
 @Injectable({
@@ -8,17 +8,10 @@ import { Category } from './category.model';
 export class CategoryService {
   constructor(private http: HttpClient) {}
 
-  header = {
-    headers: new HttpHeaders().set(
-      'Authorization',
-      `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQHRlc3QuY29tIiwiaWF0IjoxNjE3MDAzNDU5LCJleHAiOjE2MTcwMDcwNTl9.IwzvYRoFf-wsIJnzekPVKmx31ALCdT2x7RElTJs3yV8`
-    ),
-  };
-
   allCategories: Array<Category> = [];
 
   fetchCategories(): any {
-    return this.http.get<Category[]>(`/api/category/all/`);
+    return this.http.get<Category[]>(`/api/category/all`);
   }
 
   getCategoryById(id: string): any {
@@ -26,22 +19,14 @@ export class CategoryService {
   }
 
   deleteCategory(id: string): any {
-    return this.http.delete(`/api/category/delete?id=${id}`, this.header);
+    return this.http.delete(`/api/category/delete?id=${id}`);
   }
 
   addCategory(payload: Category): any {
-    return this.http.post<Category>(
-      `/api/category/create`,
-      payload,
-      this.header
-    );
+    return this.http.post<Category>(`/api/category/create`, payload);
   }
 
   updateCategory(payload: Category, id: string): any {
-    return this.http.put<Category>(
-      `/api/category/update?id=${id}`,
-      payload,
-      this.header
-    );
+    return this.http.put<Category>(`/api/category/update?id=${id}`, payload);
   }
 }
