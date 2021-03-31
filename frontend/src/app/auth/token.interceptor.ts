37640bbee7526bd8af8d861fd3147747345e0c0c
zkorra <korrawich.dev@outlook.com>
2021-03-31 10:13:12 +0700

    logout when 401

--- frontend/src/app/auth/token.interceptor.ts
+++ frontend/src/app/auth/token.interceptor.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import {
   HttpInterceptor,
@@ -7,12 +8,14 @@ import {
   HttpRequest,
   HttpHandler,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { AuthState } from './auth.state';
+import { catchError, tap } from 'rxjs/operators';
+import { Signout } from './auth.action';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private store: Store) {}
+  constructor(private store: Store, private router: Router) {}
 
   intercept(
     httpRequest: HttpRequest<any>,
@@ -20,6 +23,18 @@ export class TokenInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     const token = this.store.selectSnapshot(AuthState.token);
     const Authorization = `Bearer ${token}`;
-    return next.handle(httpRequest.clone({ setHeaders: { Authorization } }));
+    return next
+      .handle(httpRequest.clone({ setHeaders: { Authorization } }))
+      .pipe(
+        catchError((error) => {
+          console.log('dash', error);
+          if (error.status === 401) {
+            this.store.dispatch(new Signout()).subscribe(() => {
+              this.router.navigate(['/auth']);
+            });
+          }
+          return throwError(error);
+        })
+      );
   }
 }
