e7b7a8d6f241ac9422114940d389844efe1abb18
knwch <t.korrawich@protonmail.com>
2021-04-05 13:58:57 +0700

    atlas connected

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,17 @@ import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { JobModule } from './job/job.module';
 import { SkillModule } from './skill/skill.module';
+import { CLUSTER_SECRET } from './config';
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/skillguider'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/skillguider'),
+    MongooseModule.forRoot(
+      `mongodb+srv://${CLUSTER_SECRET?.username}:${CLUSTER_SECRET?.password}@cluster1412.whx5x.mongodb.net/skillguider?retryWrites=true&w=majority`,
+    ),
     AuthModule,
     CategoryModule,
     JobModule,
