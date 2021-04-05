bf7c814c62b5d63a53aaef3d2005ea6c37e16786
zkorra <korrawich.dev@outlook.com>
2021-04-05 17:35:00 +0700

    fix bug

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import mongoose from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
@@ -8,21 +9,26 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
-      useFactory: async () => ({
-        uri: `mongodb+srv://${process.env.USERNAME}:${process.env.CLUSTER_PASSWORD}@cluster1412.whx5x.mongodb.net/skillguider?retryWrites=true&w=majority`,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      }),
-    }),
-    // MongooseModule.forRoot(
-    //   `mongodb+srv://${process.env.CLUSTER_USERNAME}:${process.env.CLUSTER_PASSWORD}@cluster1412.whx5x.mongodb.net/skillguider?retryWrites=true&w=majority`,
-    // ),
+    // MongooseModule.forRootAsync({
+    //   useFactory: async () => ({
+    //     uri: `mongodb+srv://${process.env.USERNAME}:${process.env.CLUSTER_PASSWORD}@cluster1412.whx5x.mongodb.net/skillguider?retryWrites=true&w=majority`,
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //     useFindAndModify: false,
+    //     useCreateIndex: true,
+    //   }),
+    // }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.CLUSTER_USERNAME}:${process.env.CLUSTER_PASSWORD}@cluster1412.whx5x.mongodb.net/skillguider?retryWrites=true&w=majority`,
+    ),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'front'),
     }),
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schema/user.schema';
 import { AuthService } from './auth.service';
@@ -9,6 +10,7 @@ import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({
       defaultStrategy: 'jwt',
     }),
